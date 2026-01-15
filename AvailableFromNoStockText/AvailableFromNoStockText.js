bf8fb8120e5f2031165b7e365640ade56b3629bb
pkamdem <pkamdem@printequipment.de>
2026-01-15 11:47:37 +0100

    AvailableFromNoStockText updates

--- AvailableFromNoStockText/AvailableFromNoStockText.js
+++ AvailableFromNoStockText/AvailableFromNoStockText.js
@@ -2,23 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
   const body = document.body;
   const articleNumber = body.getAttribute("data-article-number");
 
-  // Nur für diesen Artikel
-  if (articleNumber !== "XT-BUNDLE-F2-UD-UV") return;
-
-  const target = document.querySelector(".w_nostock_text");
-  if (!target) return;
+  // Nur für diese Artikel
+  const allowedArticles = ["XT-BUNDLE-F2-UD-UV", "XT-BUNDLE-F2-CO-UV"];
+  if (!allowedArticles.includes(articleNumber)) return;
 
   // Sprache aus <html lang="">
   const lang = document.documentElement.lang || "de-DE";
 
-  const translations = {
-    "de-DE": "Ab 2026 lieferbar",
-    "fr-FR": "Disponible à partir de 2026",
-    "en-GB": "Available from 2026",
-  };
+  /* ----------------------------
+       Lieferhinweis Text
+    ---------------------------- */
+  const stockTarget = document.querySelector(".w_nostock_text");
+  if (stockTarget) {
+    const stockTranslations = {
+      "de-DE":
+        "Jetzt schon vorbestellen – bei Verfügbarkeit in Abhängigkeit der Bestellreihenfolge erhalten. (Lieferzeit auf Anfrage).",
+      "fr-FR":
+        "Précommandez dès maintenant – la disponibilité sera attribuée selon l’ordre des commandes. (Délai de livraison sur demande).",
+      "en-GB":
+        "Pre-order now – availability will be granted according to the order of purchase. (Delivery time upon request).",
+    };
 
-  // Fallback Deutsch
-  const text = translations[lang] || translations["de-DE"];
+    stockTarget.textContent =
+      stockTranslations[lang] || stockTranslations["de-DE"];
+  }
 
-  target.textContent = text;
+  /* ----------------------------
+       Button Text
+    ---------------------------- */
+  const buyButton = document.querySelector(".btn.btn-primary.btn-buy");
+  if (buyButton) {
+    const buttonTranslations = {
+      "de-DE": "Jetzt vorbestellen",
+      "fr-FR": "Précommander maintenant",
+      "en-GB": "Pre-order now",
+    };
+
+    const buttonText = buttonTranslations[lang] || buttonTranslations["de-DE"];
+
+    buyButton.textContent = buttonText;
+    buyButton.setAttribute("aria-label", buttonText);
+    buyButton.setAttribute("title", buttonText);
+  }
 });
